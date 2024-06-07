1bd48aeb6e2243fd61490eb88d99b50674473e52
Yahya <101402094+yo-aiv1@users.noreply.github.com>
2024-06-07 02:14:14 +0100

    improved _strlen

--- _strlen.asm
+++ _strlen.asm
@@ -2,7 +2,7 @@
 ; Function:   _strlen
 ; Purpose:    Determines the length of a C-style NUL-terminated string.
 ;
-; Prototype: size_t _strlen(const char *str);
+; Prototype: int _strlen(const char *str);
 ; 
 ; Inputs:
 ;   RCX - Pointer to the null-terminated string.
@@ -16,11 +16,9 @@ section .text
 	global _strlen      ; Entry point
 
 _strlen:
-    push    rbx         ; Push RBX to the stack (callee-saved register)
-    xor     rax, rax    ; Clear RAX (set it to 0)
-    call    start       ; Call the start label to begin the loop
-    pop     rbx         ; Restore the original value of RBX
-    ret                 ; Return from _strlen, the length is stored in RAX
+    push    rbx             ; Push RBX to the stack (callee-saved register)
+    xor     rax, rax        ; Clear RAX (set it to 0)
+    jmp     start           ; Call the start label to begin the loop
 
 start:
     cmp     byte [rcx], 0   ; Compare the byte at the memory location pointed by RCX with 0 (null terminator)
@@ -28,7 +26,9 @@ start:
                             ; If not, continue with the following instructions
     inc     rcx             ; Move to the next byte
     inc     rax             ; Increment the counter (string length)
-    call    start           ; Recursive call to continue processing the string
+    jmp     start           ; Recursive call to continue processing the string
                             ; This is a tail-recursive approach
 stop:
+    pop     rbx             ; Restore the original value of RBX
+    ret                     ; Return from _strlen, the length is stored in RAX
     ret                     ; Return to _strlen
